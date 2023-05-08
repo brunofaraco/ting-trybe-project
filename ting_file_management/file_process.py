bc94175aee4c16b2cbebe887f17cca2e83f69d3d
Bruno <brunocfaraco@hotmail.com.br>
2023-05-08 11:27:07 -0300

    feat: req 3 conclusion

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    file = txt_importer(path_file)
+    files_read = []
+
+    for i in range(instance.__len__()):
+        file_name = instance.search(i)["nome_do_arquivo"]
+        files_read.append(file_name)
+
+    if path_file not in files_read:
+        data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file),
+            "linhas_do_arquivo": file,
+        }
+
+        instance.enqueue(data)
+        sys.stdout.write(str(data))
 
 
 def remove(instance):
