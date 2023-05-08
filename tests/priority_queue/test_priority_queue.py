f90970cbd18bf22851eb83dc84bd9cfeaefc51db
Bruno <brunocfaraco@hotmail.com.br>
2023-05-08 17:00:51 -0300

    feat: req 6 conclusion

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,26 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
+
+non_priority_1 = {"qtd_linhas": 10}
+non_priority_2 = {"qtd_linhas": 6}
+
+priority = {"qtd_linhas": 3}
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    instance = PriorityQueue()
+
+    instance.enqueue(non_priority_1)
+    instance.enqueue(non_priority_2)
+
+    assert len(instance) == 2
+    assert instance.dequeue() == non_priority_1
+
+    instance.enqueue(priority)
+
+    assert instance.search(0) == priority
+
+    assert instance.dequeue() == priority
+
+    with pytest.raises(IndexError):
+        instance.search(10)
